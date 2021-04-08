e92afdc2cf94fff88d4cace0283f7aeb70efead2
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-04-08 15:37:57 +0100

    Relationship bettween Dogs and Photos

--- DogBreeders/DogBreeders/Models/Dogs.cs
+++ DogBreeders/DogBreeders/Models/Dogs.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace DogBreeders.Models {
-   
+
    /// <summary>
    /// represents the Dogs that some dog breeder has
    /// </summary>
    public class Dogs {
+
+      /// <summary>
+      /// constructor
+      /// </summary>
+      public Dogs() {
+         // access to DB, and read all Photos that the Dog has
+         Photos = new HashSet<Photos>();
+      }
+
+
 
       /// <summary>
       /// Name of dog
@@ -25,6 +35,12 @@ namespace DogBreeders.Models {
       /// </summary>
       public DateTime DateOfBirth { get; set; }
 
+      //******************************************************************
+      /// <summary>
+      ///  Dog has a list of Photos
+      /// </summary>
+      public ICollection<Photos> Photos { get; set; }
+      //******************************************************************
 
    }
 }
--- DogBreeders/DogBreeders/Models/Photos.cs
+++ DogBreeders/DogBreeders/Models/Photos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,7 +14,7 @@ namespace DogBreeders.Models {
       /// <summary>
       /// name of file that has the photo of dog 
       /// </summary>
-      public string  NameOfPhoto { get; set; }
+      public string NameOfPhoto { get; set; }
 
       /// <summary>
       /// date of photo
@@ -25,6 +26,19 @@ namespace DogBreeders.Models {
       /// </summary>
       public string Local { get; set; }
 
+      //*********************************************************
+      // making the FK from the Photo to the Dog
+      //*********************************************************
+      [ForeignKey("Dog")] // with this annotation we are connecting
+                          // attribute 'DogFK' with attribute 'Dog'
+      public int DogFK { get; set; } // mark 'Dog' as FK in DB
+
+      public Dogs Dog { get; set; } // Photo has only one Dog
+                                    // mark 'Dog' as FK in C#
+                                    // This is the only attribute that we realy nead
+                                    // the other attributs are for programmers confort
+
+      //*********************************************************
 
 
 
