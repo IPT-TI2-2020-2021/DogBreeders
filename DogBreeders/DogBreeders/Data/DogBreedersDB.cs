1dc47cf41a1620701e56a904c79a03ed222e53c1
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-04-08 16:30:38 +0100

    defining the class that is going to represent the DataBase

---
+++ DogBreeders/DogBreeders/Data/DogBreedersDB.cs
@@ -0,0 +1,26 @@
+﻿using DogBreeders.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogBreeders.Data {
+   /// <summary>
+   /// Data Base of our project
+   /// </summary>
+   public class DogBreedersDB : DbContext {
+
+
+
+      // connect our classes with the DataBase
+      public DbSet<Dogs> Dogs { get; set; }
+      public DbSet<DogBreeder> DogBreeders { get; set; }
+      public DbSet<DogBreederDogs> DogBreedersDogs { get; set; }
+      public DbSet<Photos> Photos { get; set; }
+      public DbSet<Breeds> Breeds { get; set; }
+
+   }
+}
