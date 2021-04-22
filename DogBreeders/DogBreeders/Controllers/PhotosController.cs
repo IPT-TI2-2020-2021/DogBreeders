aff43217a2eec85de32a06c954aba4023bdbbb8a
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-04-22 16:28:52 +0100

    Add of Controller+Views od Photos + customize of Index View

---
+++ DogBreeders/DogBreeders/Controllers/PhotosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DogBreeders.Data;
+using DogBreeders.Models;
+
+namespace DogBreeders.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private readonly DogBreedersDB _context;
+
+        public PhotosController(DogBreedersDB context)
+        {
+            _context = context;
+        }
+
+        // GET: Photos
+        public async Task<IActionResult> Index()
+        {
+            var dogBreedersDB = _context.Photos.Include(p => p.Dog);
+            return View(await dogBreedersDB.ToListAsync());
+        }
+
+        // GET: Photos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photos = await _context.Photos
+                .Include(p => p.Dog)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (photos == null)
+            {
+                return NotFound();
+            }
+
+            return View(photos);
+        }
+
+        // GET: Photos/Create
+        public IActionResult Create()
+        {
+            ViewData["DogFK"] = new SelectList(_context.Dogs, "Id", "Id");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NameOfPhoto,Date,Local,DogFK")] Photos photos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(photos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DogFK"] = new SelectList(_context.Dogs, "Id", "Id", photos.DogFK);
+            return View(photos);
+        }
+
+        // GET: Photos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photos = await _context.Photos.FindAsync(id);
+            if (photos == null)
+            {
+                return NotFound();
+            }
+            ViewData["DogFK"] = new SelectList(_context.Dogs, "Id", "Id", photos.DogFK);
+            return View(photos);
+        }
+
+        // POST: Photos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NameOfPhoto,Date,Local,DogFK")] Photos photos)
+        {
+            if (id != photos.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(photos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PhotosExists(photos.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DogFK"] = new SelectList(_context.Dogs, "Id", "Id", photos.DogFK);
+            return View(photos);
+        }
+
+        // GET: Photos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photos = await _context.Photos
+                .Include(p => p.Dog)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (photos == null)
+            {
+                return NotFound();
+            }
+
+            return View(photos);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var photos = await _context.Photos.FindAsync(id);
+            _context.Photos.Remove(photos);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PhotosExists(int id)
+        {
+            return _context.Photos.Any(e => e.Id == id);
+        }
+    }
+}
